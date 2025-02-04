929a58a57f064eff8857c39500ba270df9d51c08
ashiq tom <ashiqtom6@gmail.com>
2025-02-04 13:41:11 +0530

    frontend updeted

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const VITE_API_URL = `${import.meta.env.VITE_API_URL}/api`;
+console.log('VITE_API_URL', VITE_API_URL)
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: VITE_API_URL,
   withCredentials: true,
 });
