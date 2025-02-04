5b95c88c4456a06d402b568745add4326c3be192
ashiq tom <ashiqtom6@gmail.com>
2025-02-04 14:09:25 +0530

    cors problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,28 +11,31 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
 
+// If using cookies/sessions:
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("CORS Fixed!");
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
